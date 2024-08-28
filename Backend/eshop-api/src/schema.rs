b7eacd05769f662a41c85cee3de6c2836cdd970e
mdjobayerarafat <mdjobayerarafat@gmail.com>
2024-08-28 22:15:46 +0600

    schema.rs diesel

--- Backend/eshop-api/src/schema.rs
+++ Backend/eshop-api/src/schema.rs
@@ -1,4 +1,20 @@
 // @generated automatically by Diesel CLI.
+diesel::table! {
+    users (id) {
+        id -> Integer,
+        username -> Text,
+        password_hash -> Text,
+        is_admin -> Bool,
+    }
+}
+
+diesel::table! {
+    categories (id) {
+        id -> Integer,
+        name -> Text,
+    }
+}
+
 
 diesel::table! {
     products (id) {
@@ -7,5 +23,15 @@ diesel::table! {
         description -> Text,
         price -> Double,
         category_id -> Integer,
+    }
+}
+
+diesel::table! {
+    orders (id) {
+        id -> Integer,
+        user_id -> Integer,
+        product_id -> Integer,
+        total_price -> Double,
+        created_at -> Timestamp,
     }
 }

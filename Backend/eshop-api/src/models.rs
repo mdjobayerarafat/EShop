3e5a3784dea06bf625ecb4f01c223e3f393738b2
mdjobayerarafat <mdjobayerarafat@gmail.com>
2024-08-28 21:58:52 +0600

    models.rs NewCategory

--- Backend/eshop-api/src/models.rs
+++ Backend/eshop-api/src/models.rs
@@ -22,3 +22,10 @@ pub struct Category {
     pub id: i32,
     pub name: String,
 }
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name = categories)]
+pub struct NewCategory {
+    pub name: String,
+}
+
